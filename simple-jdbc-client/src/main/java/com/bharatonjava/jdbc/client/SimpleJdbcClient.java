1eb48b20f62918b3a131bdb4745d65cd4773578b
ibharatsharma <ibharatsharma@gmail.com>
2013-06-09 17:29:07 +0000

    client updated

--- simple-jdbc-client/src/main/java/com/bharatonjava/jdbc/client/SimpleJdbcClient.java
+++ simple-jdbc-client/src/main/java/com/bharatonjava/jdbc/client/SimpleJdbcClient.java
@@ -2,29 +2,75 @@ package com.bharatonjava.jdbc.client;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import com.bharatonjava.jdbc.JdbcTemplate;
 import com.bharatonjava.jdbc.SimpleJdbcTemplate;
 import com.bharatonjava.jdbc.mappers.RowMapper;
+import com.bharatonjava.transactions.DoInTransaction;
 
 public class SimpleJdbcClient {
 
 	public static void main(String[] args) throws SQLException {
 
-		JdbcTemplate template = new SimpleJdbcTemplate();
-		template.executeQuery("select * from contacts",
-				new RowMapper<Contact>() {
-
-					public Contact mapRow(ResultSet rs, int rowNum)
-							throws SQLException {
-							Contact c= new Contact();
-							c.contactId = rs.getLong("contact_id");
-							c.building = rs.getString("building");
-						return c;
-					}
-
-				});
-
+		new SimpleJdbcClient().go();
+		
 	}
 
+	public void go() throws SQLException {
+
+		JdbcTemplate template = new SimpleJdbcTemplate();
+
+		// executeInsert(template);
+		System.out.println("------------------------------------");
+		// getListOfObjects(template);
+		System.out.println("------------------------------------");
+		getSingleObject(template);
+		
+		
+	}
+
+	public void executeInsert(JdbcTemplate template) throws SQLException{
+		
+		template.executeInsert("insert into contacts (contact_id, building) values (?,?)", new Object[]{5,"Ventura"});
+	}
+	
+	public void getListOfObjects(JdbcTemplate template) throws SQLException {
+		// Query for list
+		List<Contact> lst = template.query(
+				"select contact_id, building from contacts",
+				new RowMapper<Contact>() {
+
+					@Override
+					public Contact mapRow(ResultSet rs, int rowNum)
+							throws SQLException {
+						Contact c = new Contact();
+						c.setContactId(rs.getLong("contact_id"));
+						c.setBuilding(rs.getString("building"));
+						return c;
+					}
+				});
+
+		System.out.println("List of objects"+lst);
+	}
+
+	@DoInTransaction
+	public void getSingleObject(JdbcTemplate template) throws SQLException {
+		// query for single Object
+		Contact c = template.queryForObject(
+				"select contact_id, building from contacts where contact_id=5",
+				new RowMapper<Contact>() {
+
+					@Override
+					public Contact mapRow(ResultSet rs, int rowNum)
+							throws SQLException {
+						Contact c = new Contact();
+						c.setContactId(rs.getLong("contact_id"));
+						c.setBuilding(rs.getString("building"));
+						return c;
+					}
+				});
+
+		System.out.println("Single object: " + c);
+	}
 }
