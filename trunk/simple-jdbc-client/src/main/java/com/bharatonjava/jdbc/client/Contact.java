24de4829633c1f5e8cdbfa1add012e547ee5547c
ibharatsharma@gmail.com <ibharatsharma@gmail.com@70728271-f282-4307-dc5b-5a9f89544c44>
2013-06-09 17:29:07 +0000

    client updated

--- trunk/simple-jdbc-client/src/main/java/com/bharatonjava/jdbc/client/Contact.java
+++ trunk/simple-jdbc-client/src/main/java/com/bharatonjava/jdbc/client/Contact.java
@@ -2,8 +2,34 @@ package com.bharatonjava.jdbc.client;
 
 public class Contact {
 
-	public Long contactId;
-	public String building;
+	private Long contactId;
+	private String building;
+
+	public Contact() {
+	}
+	
+	public Contact(Long contactId, String building) {
+		super();
+		this.contactId = contactId;
+		this.building = building;
+	}
+
+	public Long getContactId() {
+		return contactId;
+	}
+
+	public void setContactId(Long contactId) {
+		this.contactId = contactId;
+	}
+
+	public String getBuilding() {
+		return building;
+	}
+
+	public void setBuilding(String building) {
+		this.building = building;
+	}
+
 	@Override
 	public String toString() {
 		return "Contact [contactId=" + contactId + ", building=" + building
