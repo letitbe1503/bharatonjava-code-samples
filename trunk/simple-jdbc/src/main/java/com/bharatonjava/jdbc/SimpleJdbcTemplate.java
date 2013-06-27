7a537b81aaecc6d3a0aed178d06af8273644e5fc
ibharatsharma@gmail.com <ibharatsharma@gmail.com@70728271-f282-4307-dc5b-5a9f89544c44>
2013-06-27 10:49:33 +0000

    execute(String sql) method added to execute statements like create table etc.

--- trunk/simple-jdbc/src/main/java/com/bharatonjava/jdbc/SimpleJdbcTemplate.java
+++ trunk/simple-jdbc/src/main/java/com/bharatonjava/jdbc/SimpleJdbcTemplate.java
@@ -8,10 +8,15 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.bharatonjava.jdbc.exceptions.IncorrectResultsetSizeException;
 import com.bharatonjava.jdbc.mappers.RowMapper;
 
 public class SimpleJdbcTemplate extends JdbcTemplate {
+
+	private static final Logger log = Logger
+			.getLogger(SimpleJdbcTemplate.class);
 
 	@Override
 	protected <T> List<T> processQueryForList(Connection c, String sql,
@@ -36,13 +41,14 @@ public class SimpleJdbcTemplate extends JdbcTemplate {
 	@Override
 	protected <T> T processQueryForObject(Connection c, String sql,
 			RowMapper<T> rowMapper) throws SQLException {
+		log.info("Query: " + sql);
 
 		Statement s = c.createStatement();
 		ResultSet rs = s.executeQuery(sql);
 
 		rs.last();
 		int rowNum = rs.getRow();
-		// System.out.println(rowNum);
+		
 		if (rowNum != 1) {
 			throw new IncorrectResultsetSizeException(
 					"Resultset returned more than one rows. Expected 1, returned "
@@ -61,8 +67,9 @@ public class SimpleJdbcTemplate extends JdbcTemplate {
 	@Override
 	public int processExecuteInsert(Connection c, String sql, Object[] values)
 			throws SQLException {
-		System.out.println("Insert : " + sql);
-		
+
+		log.info("Query: " + sql);
+
 		PreparedStatement pst = c.prepareStatement(sql);
 		for (int i = 0; i < values.length; i++) {
 			if (values[i] instanceof Integer) {
@@ -71,9 +78,23 @@ public class SimpleJdbcTemplate extends JdbcTemplate {
 				pst.setString(i + 1, String.valueOf(values[i]));
 			}
 		}
+
 		int count = pst.executeUpdate();
-		System.out.println("insert count:"+ count);
+		System.out.println("insert count:" + count);
 		return count;
+	}
+
+	/**
+	 * Executes simple sql statements like create table or an insert query with
+	 * parameters appended as strings.
+	 */
+	@Override
+	public void processExecute(Connection c, String sql) throws SQLException {
+
+		log.info("Query: " + sql);
+		Statement stmt = c.createStatement();
+		boolean status = stmt.execute(sql);
+		System.out.println("status: " + status);
 	}
 
 }
