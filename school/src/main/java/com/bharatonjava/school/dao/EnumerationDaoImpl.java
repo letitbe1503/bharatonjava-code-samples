84b89031f0721e7ac1470dffb445c90139033bee
ibharatsharma <ibharatsharma@gmail.com>
2013-07-11 18:45:00 +0000

    DB Script udpated, dao classes added.

---
+++ school/src/main/java/com/bharatonjava/school/dao/EnumerationDaoImpl.java
@@ -0,0 +1,33 @@
+package com.bharatonjava.school.dao;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.bharatonjava.school.dao.mappers.EnumerationRowMapper;
+import com.bharatonjava.school.domain.Enumeration;
+
+public class EnumerationDaoImpl implements EnumerationDao{
+
+	private static final Log log = LogFactory.getLog(EnumerationDaoImpl.class);
+
+	private JdbcTemplate jdbcTemplate;
+
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	
+	public List<Enumeration> getEnumerations(String enumGroup){
+
+		String sql = "select enum_id,enum_group,enum_value from enumerations where enum_group = ?";
+		List<Enumeration> enumerations = null;
+		enumerations = this.jdbcTemplate.query(sql, new EnumerationRowMapper(), new Object[]{enumGroup});
+		log.info("Returning "+ enumerations != null? enumerations.size() : 0 + " Enumerations");
+		return enumerations;
+	}
+}
